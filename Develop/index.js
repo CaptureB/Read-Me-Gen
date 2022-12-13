4446bb597c581856d896b8ae68229bc01f169eb1
CaptureB <airdavid62@gmail.com>
2022-12-13 12:34:51 -0700

    Called inquirer,created inputs

---
+++ Develop/index.js
@@ -0,0 +1,72 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+ const generateReadme = ({title, description, installation, usage, license, contributing, tests, questions, github, email}) =>
+
+ inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of this project?',
+        },
+        {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a brief description of your project.'
+        },
+        {
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter how the user installs this application?'
+        },
+        {
+        type: 'input',
+        name: 'usage',
+        message: 'What is this application used for?'
+        },
+        {
+        type: 'checkbox',
+        name: 'license',
+        choices: ['Apache 2.0', 'MIT','Eclipise Public 2.0','Mozilla Public 2.0','None'] 
+        },
+        {
+        type: 'input',
+        name: 'contributing',
+        message: 'How should people contribute to this project?'
+        },
+        {
+        type: 'input',
+        name: 'tests',
+        message: 'How should the user test this application?'    
+        },
+        {
+        type: 'input',
+        name: 'questions',
+        message: 'Please enter any questions here.'    
+        },
+        {
+        type: 'input',
+        name: 'github',
+        message: 'Would you like to enter your Github username?'
+        },
+        {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email to be contacted if any questions arise.'  
+        },
+])
+
+.then((answers) => {
+    const readMeContent = generateReadme(answers);
+
+    fs.writeFile('README.md', readMeContent, (err) =>
+    err ? console.log(err) : console.log('Readme was created!')
+    )
+})
+
+
+
+
